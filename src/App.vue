2315e4d604292b3c8f6416412f18f4a9dd0b4b65
Natália Fonseca <nataliabf@outlook.com>
2022-03-18 14:37:02 -0300

    header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,89 @@
 <template>
   <div id="app">
-    <router-view />
+    <page-header />
+    <main id="main">
+      <router-view />
+    </main>
+    <page-footer />
   </div>
 </template>
+
+<script>
+import PageFooter from "@/components/Page Footer.vue";
+import PageHeader from "@/components/PageHeader.vue";
+
+export default {
+  components: {
+    PageHeader,
+    PageFooter,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Avenir", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", Arial, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-family: inherit;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  cursor: pointer;
+  transition: all 0.3s;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav>
+      <router-link :to="{ name: 'home' }" class="logo">
+        <img width="90" height="22" src="../assets/ranek.svg" alt="Ranek" />
+      </router-link>
+      <router-link :to="{ name: 'login' }" class="btn">
+        Vender / Login
+      </router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+
+.logo {
+  padding: 10px 0;
+}
+
+.logo img {
+  width: 90px;
+}
+</style>
